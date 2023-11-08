4af97513ceec88d9f93e639369a62e9cec8efa3e
Sarah Cochrane <sarah.kristine.cochrane@gmail.com>
2023-11-07 22:20:51 -0800

    Add calculator buttons

--- javascript.js
+++ javascript.js
@@ -3,6 +3,23 @@
 //      Value 2
 //      Operator
 //      Buttons
+let oneBtn = document.querySelector("#one-btn");
+let twoBtn = document.querySelector("#two-btn");
+let threeBtn = document.querySelector("#three-btn");
+let fourBtn = document.querySelector("#four-btn");
+let fiveBtn = document.querySelector("#five-btn");
+let sixBtn = document.querySelector("#six-btn");
+let sevenBtn = document.querySelector("#seven-btn");
+let eightBtn = document.querySelector("#eight-btn");
+let nineBtn = document.querySelector("#nine-btn");
+let zeroBtn = document.querySelector("#zero-btn");
+let clearBtn = document.querySelector("#clear-btn");
+let deleteBtn = document.querySelector("#delete-btn");
+let plusBtn = document.querySelector("#plus-btn");
+let minusBtn = document.querySelector("#minus-btn");
+let multBtn = document.querySelector("#mult-btn");
+let divBtn = document.querySelector("#div-btn");
+let equalBtn = document.querySelector("#equal-btn");
 //      Display
 let display = document.querySelector(".display");
 
